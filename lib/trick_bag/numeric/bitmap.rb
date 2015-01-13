94541c1da1598804e747920a48b164601431a3ef
Keith R. Bennett <keithrbennett@gmail.com>
2015-01-12 21:51:25 -0500

    Flesh out number to other format methods.

--- lib/trick_bag/numeric/bitmap.rb
+++ lib/trick_bag/numeric/bitmap.rb
@@ -1,85 +1,150 @@
 module TrickBag
 module Numeric
 
-  class Bitmap
 
-    # This method is made private to enforce that the more descriptively
-    # named class methods should be used to create instances!
-    private_class_method :new
+# Provides class methods for converting between the various representations
+# of a bitmap: number, binary encoded string, array, and sparse array.
+#
+# An instance can be created that will hold on to bitmap data and be used
+# to test bits and convert to other formats.
+class Bitmap
+
+  # The constructor is made private because:
+  #
+  # 1) each type of initialization requires its own validation, and it
+  #    would be wasteful to do the validation unnecessarily
+  # 2) to enforce that the more descriptively
+  #    named class methods should be used to create instances.
+  private_class_method :new
 
 
-    # def f(string)
-    #   string = string.clone.force_encoding(Encoding::ASCII_8BIT) unless string.encoding == Encoding::ASCII_8BIT
-    #   string.bytes.inject(0) { |number, byte| puts "\nbyte: #{byte.ord}, number = #{number}\n"; number * 256 + byte.ord }
-    # end
-    def self.binary_string_to_number(string)
-      string = string.clone.force_encoding(Encoding::ASCII_8BIT) unless string.encoding == Encoding::ASCII_8BIT
-      string.bytes.inject(0) { |number, byte| puts "\nbyte: #{byte.ord}, number = #{number}\n"; number * 256 + byte.ord }
+  # Class methods for converting between representations:
+
+  def self.binary_string_to_number(string)
+    string = string.clone.force_encoding(Encoding::ASCII_8BIT)
+    string.bytes.inject(0) do |number, byte|
+      number * 256 + byte.ord
+    end
+  end
+
+
+  def self.assert_nonnegative(number)
+    raise ArgumentError.new("Number was #{number} but must >= 0") if number < 0
+  end
+  private_class_method :assert_nonnegative
+
+
+  # Converts a number to an array of place values, e.g. 9 => [8, 0, 0, 1]
+  def self.number_to_value_array(number)
+    assert_nonnegative(number)
+    array = []
+    bit_value = 1
+    while number > 0
+      array << ((number & 1 == 1) ? bit_value : 0)
+      number >>= 1
+      bit_value <<= 1
+    end
+    array.reverse
+  end
+
+  # Converts a number to an array of bit values, e.g. 9 => [1, 0, 0, 1]
+  def self.number_to_bit_array(number)
+    assert_nonnegative(number)
+    array = []
+    while number > 0
+      array << (number & 1)
+      number >>= 1
+    end
+    array.reverse
+  end
+
+
+  # Converts a number to a sparse array containing bit positions that are set/true/1.
+  # Note that these are bit positions, e.g. 76543210, and not bit column values
+  # such as 128/64/32/16/8/4/2/1.
+  def self.number_to_set_bit_positions_array(number)
+    assert_nonnegative(number)
+    array = []
+    position = 0
+    while number > 0
+      array << position if number & 1 == 1
+      position += 1
+      number >>= 1
+    end
+    array
+  end
+
+  # Converts a number to a binary encoded string, e.g. 256 => "\x01\x00"
+  def self.number_to_binary_string(number)
+    assert_nonnegative(number)
+    binary_string = ''.force_encoding(Encoding::ASCII_8BIT)
+
+    while number > 0
+      byte_value = number & 0xFF
+      binary_string << byte_value
+      number >>= 8
     end
 
-    def self.number_to_bit_array(number)
-      array = []
-      while number > 0
-        array << (number & 1)
-        number >>= 1
-      end
-      array.reverse
+    binary_string.reverse
+  end
+
+
+  def self.binary_string_to_bit_value_array(string)
+    number = binary_string_to_number(string)
+    number_to_bit_array(number)
+  end
+
+  # Class methods to create instances from the various representation types:
+
+  # def self.from_binary_string(string)
+  #   binary_string = string.clone.force_encoding('ASCII-8BIT')
+  #   from_array(binary_string_to_array(binary_string))
+  # end
+
+  def self.from_array(array)
+    new(array)
+  end
+
+  def self.from_number(number)
+    assert_nonnegative(number)
+    unless [Fixnum, Bignum].include?(number.class)
+      raise ArgumentError("#{number} is a #{number.class}, not a Fixnum or Bignum")
     end
-    # def g(number)
-    #   array = []
-    #   while number > 0
-    #     array << (number & 1)
-    #     number >>= 1
-    #   end
-    #   array.reverse
-    # end
+  end
 
-    def self.binary_string_to_array(string)
-      number = binary_string_to_number(string)
-      number_to_bit_array(number)
+
+  def self.from_sparse_array
+
+  end
+
+  def on?(n)
+
+  end
+
+  def off?(n)
+
+  end
+
+  # @return the number of bits in the string; assumes that the string's
+  #         chars are all 1 byte in length, as in Encoding::ASCII_8BIT.
+  def self.bit_count(string)
+    string.size * 8
+  end; private_class_method :new, :bit_count
+
+
+  def initialize(number)
+    @number = number
+  end
+
+  def &(bit)
+    unless bit % 2 == 0
+      raise ArgumentError.new("Bit was #{bit} but must be a multiple of 2.")
     end
-
-    def self.from_binary_string(string)
-      binary_string = string.clone.force_encoding('ASCII-8BIT')
-      from_array(binary_string_to_array(binary_string))
-    end
-
-    def self.from_array(array)
-      new(array)
-    end
-
-    def self.from_sparse_array
-
-    end
-
-    def to_binary_string
-
-    end
-
-    def to_array
-
-    end
-
-    def to_sparse_array
-
-    end
-
-    def on?(n)
-
-    end
-
-    def off?(n)
-
-    end
-
-    def self.bit_count(string)
-
-    end; private :bit_count
-
-    def initialize(array)
-      @array = array
-    end
+    @number & bit
   end
 
 end
 end
+end
+
+
--- spec/trick_bag/numeric/bitmap_spec.rb
+++ spec/trick_bag/numeric/bitmap_spec.rb
@@ -5,18 +5,107 @@ require 'trick_bag/numeric/bitmap'
 module TrickBag
 module Numeric
 
+
+  raw_string = ->(s) { s.force_encoding(Encoding::ASCII_8BIT) }
+
   describe Bitmap do
+
 
     specify 'the constructor should not permit being explicitly called outside the class' do
       expect(-> { Bitmap.new }).to raise_error
-      # expect { Bitmap.new }.to raise_error
+    end
+
+
+    context '.binary_string_to_number' do
+
+      it 'computes byte values correctly' do
+        expect(Bitmap.binary_string_to_number("\xFF")).to eq(255)
+        expect(Bitmap.binary_string_to_number("\x0D")).to eq(13)
+      end
+
+      it 'considers the left-most byte most significant' do
+        expect(Bitmap.binary_string_to_number("\x01\x00")).to eq(256)
+      end
+
+      # Unspecified encoding results in UTF-8 anyway, but this makes it explicit:
+      it 'correctly handles non-ASCII_8BIT strings' do
+        expect(Bitmap.binary_string_to_number("\x01\x00".force_encoding(Encoding::UTF_8))).to eq(256)
+      end
+    end
+  end
+
+
+  context '.number_to_value_array' do
+
+    specify 'a single bit set whose value == 9 results in [8, 0, 0, 1]' do
+      expect(Bitmap.number_to_value_array(9)).to eq([8, 0, 0, 1])
+    end
+
+    specify 'a single bit set whose value == 256 results in [256] + [0] * 8' do
+      expect(Bitmap.number_to_value_array(256)).to eq([256] + [0] * 8)
+    end
+
+    specify 'a negative number should result in an error' do
+      expect { Bitmap.number_to_value_array(-1) }.to raise_error(ArgumentError)
+    end
+  end
+
+  context '.number_to_bit_array' do
+
+    specify 'a single bit set whose value == 9 results in [1, 0, 0, 1]' do
+      expect(Bitmap.number_to_bit_array(9)).to eq([1, 0, 0, 1])
+    end
+
+    specify 'a single bit set whose value == 256 results in [1] + [0] * 8' do
+      expect(Bitmap.number_to_bit_array(256)).to eq([1] + [0] * 8)
+    end
+
+    specify 'a negative number should result in an error' do
+      expect { Bitmap.number_to_bit_array(-1) }.to raise_error(ArgumentError)
+    end
+  end
+
+
+  context '.binary_string_to_bit_value_array' do
+    it 'computes byte values correctly' do
+      expect(Bitmap.binary_string_to_bit_value_array("\xFF")).to eq([1] * 8)
+      expect(Bitmap.binary_string_to_bit_value_array("\x0D")).to eq([1,1,0,1])
+    end
+
+    it 'considers the left-most byte most significant' do
+      expect(Bitmap.binary_string_to_bit_value_array("\x01\x00")).to eq([1] + [0] * 8)
+    end
+
+    # Unspecified encoding results in UTF-8 anyway, but this makes it explicit:
+    it 'correctly handles non-ASCII_8BIT strings' do
+      expect(Bitmap.binary_string_to_bit_value_array("\x01\x00".force_encoding(Encoding::UTF_8))).to eq([1] + [0] * 8)
+    end
+  end
+
+
+  context '.number_to_binary_string' do
+
+    specify '256 converted to a binary string is "\x01\00"' do
+      expect(Bitmap.number_to_binary_string(256)).to eq("\x01\x00")
+    end
+
+    specify 'a negative number should result in an error' do
+      expect { Bitmap.number_to_binary_string(-1) }.to raise_error(ArgumentError)
     end
 
   end
 
+  describe '.number_to_set_bit_positions_array' do
 
+    specify "9's set bit positions should be [0, 3]" do
+      expect(Bitmap.number_to_set_bit_positions_array(9)).to eq([0, 3])
+    end
 
+    specify 'a negative number should result in an error' do
+      expect { Bitmap.number_to_set_bit_positions_array(-1) }.to raise_error(ArgumentError)
+    end
+  end
 
-end
+  end
 end
 
