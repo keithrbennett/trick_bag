2611f7dab5dc7fd34cd0c7bb9b4b7cd6e1d8a660
Keith R. Bennett <keithrbennett@gmail.com>
2015-06-11 06:49:25 -0400

    SshOutputReader: add latest_output_type, channel attributes.  Add close method.

--- lib/trick_bag/networking/ssh_output_reader.rb
+++ lib/trick_bag/networking/ssh_output_reader.rb
@@ -13,7 +13,7 @@ require 'net/ssh'
 class SshOutputReader
 
   attr_reader :host, :command, :user, :password, :exit_predicate
-  attr_accessor :latest_stdout, :all_stdout, :latest_stderr, :all_stderr
+  attr_accessor :latest_stdout, :all_stdout, :latest_stderr, :all_stderr, :latest_output_type, :channel
 
   def initialize(host, command, user = ENV['USER'], password = nil)
     @host = host
@@ -33,12 +33,14 @@ class SshOutputReader
   end
 
 
-  # Sets a predicate that, when called with self, returns true
-  # to close the channel and return, or returns false to permit the
+  # Sets a predicate that, when called with this reader instance, returns true
+  # to close the channel and return, or false to permit the
   # channel to continue operating.
   #
   # Using this self value, any instance methods can be called; the most useful
   # will probably be latest_stdout, all_stdout, latest_stderr, and all_stderr.
+  # There is also a latest_output_type that returns :stdout or :stderr to
+  # indicate which type of output just occurred.
   #
   # For example, to exit when the string "exit" is sent to stdout:
   # reader.set_exit_predicate(->(reader) { /exit/ === reader.latest_stdout })
@@ -63,12 +65,13 @@ class SshOutputReader
 
       # Open a new channel and configure a minimal set of callbacks, then run
       # the event loop until the channel finishes (closes).
-      channel = ssh.open_channel do |ch|
+      self.channel = ssh.open_channel do |ch|
         ch.exec(command) do |ch, success|
           raise "could not execute command" unless success
 
           # "on_data" is called when the process writes something to stdout
           ch.on_data do |c, data|
+            self.latest_output_type = :stdout
             self.latest_stdout = data
             self.all_stdout << data
             $stdout.print data
@@ -77,19 +80,29 @@ class SshOutputReader
 
           # "on_extended_data" is called when the process writes something to stderr
           ch.on_extended_data do |c, type, data|
+            self.latest_output_type = :stderr
             self.latest_stderr = data
             self.all_stderr << data
             $stderr.print data
             ch.close if exit_predicate.(self)
           end
 
-          ch.on_close { return self }
+          ch.on_close { self.channel = nil; return self }
         end
       end
 
       channel.wait
+      self.channel = nil
       self
     end
+  end
+
+
+  # The channel will automatically be closed if the predicate returns false
+  # or if the command completes.  This method is provided for cases where
+  # the predicate wants to raise an error; close should be called before raising the error.
+  def close
+    channel.close if channel
   end
 
 
@@ -104,7 +117,12 @@ if $0 == __FILE__
 
   Thread.abort_on_exception = true
   # require 'pry'
-  has_a_2 = ->(reader) { /2/ === reader.latest_stdout }
+  has_a_2 = ->(reader) do
+    reader.latest_output_type == :stdout \
+    && \
+    /2/ === reader.latest_stdout
+  end
+
   has_3_lines = ->(reader) { reader.all_stdout.split("\n").size >= 3 }
 
   def run_reader(r)
@@ -115,18 +133,25 @@ if $0 == __FILE__
     puts "Done\n#{'-' * 79}\n"
   end
 
-  command = 'echo 1; sleep 1; echo 2; sleep 1; echo 3; sleep 1; echo 4'
-  # command = 'ls'
+  command = \
+'echo 1
+sleep 1
+echo error output >& 2
+echo 2
+sleep 1
+echo 3
+sleep 1
+echo 4'
 
   # reader = SshOutputReader.new('localhost', command)
   # reader.run
   # puts "All reader stdout:\n" + reader.all_stdout
 
-  # run_reader(SshOutputReader.new('localhost', command)\
-  #     .set_exit_predicate(has_a_2))
-  #
   run_reader(SshOutputReader.new('localhost', command)\
-      .set_exit_predicate(has_3_lines))
+      .set_exit_predicate(has_a_2))
+
+  # run_reader(SshOutputReader.new('localhost', command)\
+  #     .set_exit_predicate(has_3_lines))
 
   # run_reader(SshOutputReader.new('localhost', command))
 end
